693e7de2285c8d14c11445c98fee58bfbb2b3049
Julia Becker <juliabecker-kronau@web.de>
2019-03-27 11:17:01 +0100

    Packages

--- src/main/java/dhbwka/wwi/vertsys/javee/filmsortierung/filme/ejb/GenreBean.java
+++ src/main/java/dhbwka/wwi/vertsys/javee/filmsortierung/filme/ejb/GenreBean.java
@@ -1,19 +1,28 @@
 package dhbwka.wwi.vertsys.javee.filmsortierung.filme.ejb;
 
-/*
- * Copyright © 2019 Dennis Schulmeister-Zimolong
- *
- * E-Mail: dhbw@windows3.de
- * Webseite: https://www.wpvs.de/
- *
- * Dieser Quellcode ist lizenziert unter einer
- * Creative Commons Namensnennung 4.0 International Lizenz.
- */
+import dhbwka.wwi.vertsys.javaee.filmsortierung.common.ejb.EntityBean;
+import dhbwka.wwi.vertsys.javaee.filmsortierung.tasks.jpa.Category;
+import java.util.List;
+import javax.annotation.security.RolesAllowed;
+import javax.ejb.Stateless;
 
 /**
- *
- * @author simon
+ * Einfache EJB mit den üblichen CRUD-Methoden für Kategorien.
  */
-public class GenreBean {
+@Stateless
+@RolesAllowed("app-user")
+public class GenreBean extends EntityBean<Category, Long> {
 
+    public GenreBean() {
+        super(Category.class);
+    }
+
+    /**
+     * Auslesen aller Kategorien, alphabetisch sortiert.
+     *
+     * @return Liste mit allen Kategorien
+     */
+    public List<Category> findAllSorted() {
+        return this.em.createQuery("SELECT c FROM Category c ORDER BY c.name").getResultList();
+    }
 }
