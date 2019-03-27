6f4e1d573fd8ce85817f9abe6dc33ed0a561ccc7
Julia Becker <juliabecker-kronau@web.de>
2019-03-27 13:44:34 +0100

    Änderungen User

--- src/main/java/dhbwka/wwi/vertsys/javaee/filmsortierung/dashboard/web/DashboardServlet.java
+++ src/main/java/dhbwka/wwi/vertsys/javaee/filmsortierung/dashboard/web/DashboardServlet.java
@@ -28,7 +28,7 @@ import javax.servlet.http.HttpServletResponse;
 public class DashboardServlet extends HttpServlet {
 
     // Kacheln für Aufgaben
-    @EJB(beanName = "tasks")
+    @EJB(beanName = "films")
     DashboardContentProvider taskContent;
 
     @Override
