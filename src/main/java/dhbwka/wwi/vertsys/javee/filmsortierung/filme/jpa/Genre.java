c300b35e229b19ca9b791a84f1ae7c8b4ac5efa4
SimonRothmann <simon.rothmann@arcor.de>
2019-03-27 10:56:05 +0100

    created new packages and empty classes

---
+++ src/main/java/dhbwka/wwi/vertsys/javee/filmsortierung/filme/jpa/Genre.java
@@ -0,0 +1,19 @@
+package dhbwka.wwi.vertsys.javee.filmsortierung.filme.jpa;
+
+/*
+ * Copyright © 2019 Dennis Schulmeister-Zimolong
+ *
+ * E-Mail: dhbw@windows3.de
+ * Webseite: https://www.wpvs.de/
+ *
+ * Dieser Quellcode ist lizenziert unter einer
+ * Creative Commons Namensnennung 4.0 International Lizenz.
+ */
+
+/**
+ *
+ * @author simon
+ */
+public class Genre {
+
+}
---
+++ src/main/java/dhbwka/wwi/vertsys/javee/filmsortierung/filme/web/GenreListServlet.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright © 2019 Dennis Schulmeister-Zimolong
+ *
+ * E-Mail: dhbw@windows3.de
+ * Webseite: https://www.wpvs.de/
+ *
+ * Dieser Quellcode ist lizenziert unter einer
+ * Creative Commons Namensnennung 4.0 International Lizenz.
+ */
+package dhbwka.wwi.vertsys.javee.filmsortierung.filme.web;
+
+/**
+ *
+ * @author simon
+ */
+public class GenreListServlet {
+
+}
