e54baef4ae835429993cbb2445f0a0bb36ee341c
vibach <43773192+vibach@users.noreply.github.com>
2019-03-29 11:09:29 +0100

    Rest Authentifizierung

--- src/main/java/dhbwka/wwi/vertsys/javaee/filmsortierung/filme/rest/SucheFilmRest.java
+++ src/main/java/dhbwka/wwi/vertsys/javaee/filmsortierung/filme/rest/SucheFilmRest.java
@@ -10,13 +10,16 @@
 package dhbwka.wwi.vertsys.javaee.filmsortierung.filme.rest;
 
 import com.google.gson.Gson;
+import dhbwka.wwi.vertsys.javaee.filmsortierung.common.ejb.UserBean;
+import dhbwka.wwi.vertsys.javaee.filmsortierung.common.jpa.User;
 import dhbwka.wwi.vertsys.javee.filmsortierung.filme.ejb.FilmBean;
 import dhbwka.wwi.vertsys.javee.filmsortierung.filme.jpa.Film;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
 
 /**
  *
@@ -30,12 +33,41 @@ public class SucheFilmRest {
     @EJB
     private FilmBean filmBean;
 
+    @EJB
+    private UserBean userBean;
+
     @GET
-    public String doGet() {
-        List<Film> filme = this.filmBean.findAll();
-        Gson gson = new Gson();
-        String json = gson.toJson(filme);
-        return json;
+    public String doGet(@HeaderParam("Authorization") String authorization) {
+        // Anzuzeigende Spezies suchen
+        if (authorization != null && authorization.toLowerCase().startsWith("basic")) {
+            // Authorization: Basic base64credentials
+            String base64Credentials = authorization.substring("Basic".length()).trim();
+            byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
+            String credentials = new String(credDecoded, StandardCharsets.UTF_8);
+            // credentials = username:password
+            final String[] values = credentials.split(":", 2);
+
+            System.out.println("");
+            System.out.println(values[0] + " bla " + values[1]);
+
+            // Error: 0 = kein User; 2 = falsches Passwort
+            List<User> user = userBean.findUser(values[0]);
+            if (user.size() < 1) {
+                return "0";
+            }
+
+            if (user.get(0).checkPassword(values[1])) {
+                List<Film> filme = this.filmBean.findAll();
+                Gson gson = new Gson();
+                String json = gson.toJson(filme);
+                return json;
+            } else {
+                return "2";
+            }
+        }
+
+        return null;
+
     }
 
 }
