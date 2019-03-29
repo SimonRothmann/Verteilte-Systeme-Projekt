ab81fa8270cd8adf020d98a67f71e46b7a7a3e68
Julia Becker <juliabecker-kronau@web.de>
2019-03-29 11:22:12 +0100

    sucheGenreRest - Änderung

--- src/main/java/dhbwka/wwi/vertsys/javaee/filmsortierung/filme/rest/SucheGenreRest.java
+++ src/main/java/dhbwka/wwi/vertsys/javaee/filmsortierung/filme/rest/SucheGenreRest.java
@@ -10,9 +10,12 @@
 package dhbwka.wwi.vertsys.javaee.filmsortierung.filme.rest;
 
 import com.google.gson.Gson;
+import dhbwka.wwi.vertsys.javaee.filmsortierung.common.ejb.UserBean;
+import dhbwka.wwi.vertsys.javaee.filmsortierung.common.jpa.User;
 import dhbwka.wwi.vertsys.javee.filmsortierung.filme.ejb.GenreBean;
 import dhbwka.wwi.vertsys.javee.filmsortierung.filme.jpa.Genre;
-import java.util.List;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.*;
@@ -23,16 +26,42 @@ import javax.ws.rs.*;
  */
 @Stateless
 @Path("sucheGenre")
+
 public class SucheGenreRest {
 
     @EJB
     private GenreBean genreBean;
 
+    @EJB
+    private UserBean userBean;
+
     @GET
-    public String doGet() {
-        List<Genre> genre = this.genreBean.findAll();
-        Gson gson = new Gson();
-        String json = gson.toJson(genre);
-        return json;
+    public String doGet(@HeaderParam("Authorization") String authorization) {
+
+        if (authorization != null && authorization.toLowerCase().startsWith("basic")) {
+            // Authorization: Basic base64credentials
+            String base64Credentials = authorization.substring("Basic".length()).trim();
+            byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
+            String credentials = new String(credDecoded, StandardCharsets.UTF_8);
+            // credentials = username:password
+            final String[] values = credentials.split(":", 2);
+
+            // Error: 0 = kein User; 2 = falsches Passwort
+            List<User> user = userBean.findUser(values[0]);
+            if (user.size() < 1) {
+                return "0";
+            }
+
+            if (user.get(0).checkPassword(values[1])) {
+                List<Genre> genre = this.genreBean.findAll();
+                Gson gson = new Gson();
+                String json = gson.toJson(genre);
+                return json;
+            } else {
+                return "2";
+            }
+        }
+        return null;
+
     }
 }
